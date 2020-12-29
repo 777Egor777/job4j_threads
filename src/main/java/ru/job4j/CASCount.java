badc165e5e30f764d2a9c43f5e26c0454ad1c543
Egor <yegeraskin13@gmail.com>
2020-12-29 12:28:56 +0400

    0. CAS - операции [#435459] (AtomicInteger, work)

--- src/main/java/ru/job4j/CASCount.java
+++ src/main/java/ru/job4j/CASCount.java
@@ -12,6 +12,7 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 @ThreadSafe
 public class CASCount {
+    /*
     private final AtomicReference<AtomicInteger> count = new AtomicReference<>(new AtomicInteger(0));
 
     public void increment() {
@@ -30,5 +31,75 @@ public class CASCount {
             currentValue = count.get();
         } while (!count.compareAndSet(currentValue, currentValue));
         return currentValue.get();
+    }
+     */
+    private final AtomicInteger count = new AtomicInteger();
+
+    public void increment() {
+        count.incrementAndGet();
+    }
+
+    public int get() {
+        return count.get();
+    }
+/*
+    public static void main(String[] args) {
+        CASCount count = new CASCount();
+        Thread threadA = new Thread(
+                () -> {
+                    for (int i = 0; i < 50; ++i) {
+                        count.increment();
+                    }
+                }
+        );
+        Thread threadB = new Thread(
+                () -> {
+                    for (int i = 0; i < 50; ++i) {
+                        count.increment();
+                    }
+                }
+        );
+        Thread threadC = new Thread(
+                () -> {
+                    for (int i = 0; i < 50; ++i) {
+                        count.increment();
+                    }
+                }
+        );
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        try {
+            threadA.join();
+            threadB.join();
+            threadC.join();
+        } catch (InterruptedException iex) {
+            iex.printStackTrace();
+        }
+        System.out.println(count.get());
+    }
+
+ */
+    public static void main(String[] args) {
+        CASCount count = new CASCount();
+        final int n = 150;
+        Thread[] thread = new Thread[n];
+        for (int i = 0; i < n; ++i) {
+            thread[i] = new Thread(
+                    count::increment
+            );
+        }
+        for (int i = 0; i < n; ++i) {
+            thread[i].start();
+        }
+
+        try {
+            for (int i = 0; i < n; ++i) {
+                thread[i].join();
+            }
+        } catch (InterruptedException iex) {
+            iex.printStackTrace();
+        }
+        System.out.println(count.get());
     }
 }
