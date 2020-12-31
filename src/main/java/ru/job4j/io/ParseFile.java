eb58c22ce74577c7835bd4315d66c36f4b2291db
Egor <yegeraskin13@gmail.com>
2020-12-31 14:48:05 +0400

    1. Visibility. Общий ресурс вне критической секции [#435451] (Refactor #1)

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -1,45 +1,60 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author Geraskin Egor
  * @version 1.0
  * @since 26.12.2020
  */
-public class ParseFile {
-    private volatile File file;
+public final class ParseFile {
+    private final File file;
 
-    public synchronized void setFile(File f) {
-        file = f;
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized ParseFile setFile(File f) {
+        return new ParseFile(f);
     }
 
     public synchronized File getFile() {
         return file;
     }
 
-    public synchronized String getContent() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
-    }
-
-    public synchronized String getContentWithoutUnicode() throws IOException {
-        InputStreamReader reader = new InputStreamReader(new FileInputStream(file));
-        int ch;
-        StringBuffer buffer = new StringBuffer();
-        while ((ch = reader.read()) >= 0) {
-            if (ch < 0x80) {
-                buffer.append((char) ch);
-            }
+    public synchronized String getContent() {
+        String content = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            content = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
         }
-        return buffer.toString();
+        return content;
     }
 
-    public synchronized void saveContent(String content) throws IOException {
-        PrintStream printer = new PrintStream(file);
-        printer.print(content);
+    public synchronized String getContentWithoutUnicode() {
+        String content = "";
+        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(file))) {
+            int ch;
+            StringBuffer buffer = new StringBuffer();
+            while ((ch = reader.read()) >= 0) {
+                if (ch < 0x80) {
+                    buffer.append((char) ch);
+                }
+            }
+            content = buffer.toString();
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+        }
+        return content;
+    }
+
+    public synchronized void saveContent(String content) {
+        try (PrintStream printer = new PrintStream(file)) {
+            printer.print(content);
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+        }
     }
 }
