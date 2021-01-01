05ee32d776bee74e9b9bfd5531f9bbb2e1a0756d
Egor <yegeraskin13@gmail.com>
2021-01-01 12:31:40 +0400

    1. Visibility. Общий ресурс вне критической секции [#435451] (Refactor #2)

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -23,31 +24,35 @@ public final class ParseFile {
         return file;
     }
 
-    public synchronized String getContent() {
+    public String filterLine(String line, Predicate<Integer> pred) {
+        StringBuffer buffer = new StringBuffer();
+        for (int index = 0; index < line.length(); ++index) {
+            char c = line.charAt(index);
+            if (pred.test((int) c)) {
+                buffer.append(c);
+            }
+        }
+        return buffer.toString();
+    }
+
+    public synchronized String getContent(Predicate<Integer> pred) {
         String content = "";
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            content = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            content = reader.lines()
+                    .map(str -> filterLine(str, pred))
+                    .collect(Collectors.joining(System.lineSeparator()));
         } catch (IOException ioEx) {
             ioEx.printStackTrace();
         }
         return content;
     }
 
-    public synchronized String getContentWithoutUnicode() {
-        String content = "";
-        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(file))) {
-            int ch;
-            StringBuffer buffer = new StringBuffer();
-            while ((ch = reader.read()) >= 0) {
-                if (ch < 0x80) {
-                    buffer.append((char) ch);
-                }
-            }
-            content = buffer.toString();
-        } catch (IOException ioEx) {
-            ioEx.printStackTrace();
-        }
-        return content;
+    public String getContent() {
+        return getContent(i -> true);
+    }
+
+    public String getContentWithoutUnicode() {
+        return getContent(i -> i < 0x80);
     }
 
     public synchronized void saveContent(String content) {
